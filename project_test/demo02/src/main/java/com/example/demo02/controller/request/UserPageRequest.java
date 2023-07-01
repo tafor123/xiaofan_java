8ff4cd6ff9fecdcb1665a2506c0bcf8250031c4c
小凡 <3276139871@qq.com>
2023-07-01 23:45:00 +0800

    图书管理系统

--- project_test/demo02/src/main/java/com/example/demo02/controller/request/UserPageRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo02.controller.request;
-
-import lombok.Data;
-
-@Data
-public class UserPageRequest extends BaseRequest {
-  private String name;
-  private String phone;
-}
