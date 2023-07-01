06740d9bf973103c950241265e6ecbcbd75b5aa4
小凡 <3276139871@qq.com>
2023-07-02 00:31:25 +0800

    图书管理系统

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import '@/assets/global.css'
+import SlideVerify from 'vue-monoplasty-slide-verify';
 
+
+
+Vue.use(SlideVerify);
 Vue.config.productionTip = false
+Vue.use(ElementUI, {size:'small'});
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,133 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Layout from '../views/Layout.vue'
+import Cookies from "js-cookie";
 
 Vue.use(VueRouter)
 
 const routes = [
+  //  ======  登录  ======
+  {
+    path: '/login',
+    name: 'Login',
+    component: ()=> import('@/views/login/Login.vue'),
+  },
+
+  //  ======   主页 ======
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Layout',
+    component: Layout,
+    redirect: '/home',
+    children: [ //子路由
+
+      //  ======    首页 ======
+      {
+        path: 'home',
+        name: 'Home',
+        component: () => import('@/views/home/HomeView.vue'),
+      },
+
+      //  ======    User ======
+      {
+        path: 'userList',
+        name: 'UserList',
+        component: () => import('@/views/user/User.vue'),
+      },
+      {
+        path: 'addUser',
+        name: 'AddUser',
+        component: () => import('@/views/user/AddUser.vue'),
+      },
+      {
+        path: 'editUser',
+        name: 'EditUser',
+        component: () => import('@/views/user/EditUser.vue'),
+      },
+
+      //======    Admin ======
+      {
+        path: 'adminList',
+        name: 'AdminList',
+        component: () => import('@/views/admin/List.vue'),
+      },
+      {
+        path: 'addAdmin',
+        name: 'AddAdmin',
+        component: () => import('@/views/admin/Add.vue'),
+      },
+      {
+        path: 'editAdmin',
+        name: 'EditAdmin',
+        component: () => import('@/views/admin/Edit.vue'),
+      },
+
+      //======    Category ======
+      {
+        path: 'categoryList',
+        name: 'CategoryList',
+        component: () => import('@/views/category/List.vue'),
+      },
+      {
+        path: 'addCategory',
+        name: 'AddCategory',
+        component: () => import('@/views/category/Add.vue'),
+      },
+      {
+        path: 'editCategory',
+        name: 'EditCategory',
+        component: () => import('@/views/category/Edit.vue'),
+      },
+
+      //======    Book   ======
+      {
+        path: 'bookList',
+        name: 'BookList',
+        component: () => import('@/views/book/List.vue'),
+      },
+      {
+        path: 'addBook',
+        name: 'AddBook',
+        component: () => import('@/views/book/Add.vue'),
+      },
+      {
+        path: 'editBook',
+        name: 'EditBook',
+        component: () => import('@/views/book/Edit.vue'),
+      },
+
+      //======    Borrow   ======
+      {
+        path: 'borrowList',
+        name: 'BorrowList',
+        component: () => import('@/views/borrow/List.vue'),
+      },
+      {
+        path: 'addBorrow',
+        name: 'AddBorrow',
+        component: () => import('@/views/borrow/Add.vue'),
+      },
+      {
+        path: 'editBorrow',
+        name: 'EditBorrow',
+        component: () => import('@/views/borrow/Edit.vue'),
+      },
+      //======    Return   ======
+      {
+        path: 'returList',
+        name: 'ReturList',
+        component: () => import('@/views/retur/List.vue'),
+      },
+    ]
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: "*",
+    component:() => import('@/views/404')
+
   }
 ]
+
+
 
 const router = new VueRouter({
   mode: 'history',
@@ -26,4 +135,11 @@ const router = new VueRouter({
   routes
 })
 
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') next()
+  const admin = Cookies.get("admin")
+  if (!admin && to.path !== '/login') return next("/login") //强制退回登录页面
+  // 访问 /home 的时候，并且cookie里面有数据，这个时候放行
+  next()
+})
 export default router
